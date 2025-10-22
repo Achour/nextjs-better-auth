df46dc69cd78e0434ec647e80dc4eb4421fe4ff4
achour <achourphp@gmail.com>
2025-10-22 22:23:08 +0100

    refactor: convert login and signup page components to async functions for session handling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,23 @@
 import { LoginForm } from "@/components/auth/login-form";
 
-export default function Page() {
+
+import { auth } from "@/lib/auth"; // path to your Better Auth server instance
+import { headers } from "next/headers";
+
+import { redirect } from "next/navigation";
+
+
+export default async function Page() {
+
+
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+
+  if (session?.user) {
+    redirect('/dashboard');
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
